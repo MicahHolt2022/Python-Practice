4b64941b35211ed3ef1dd44ef25e26ab0714b852
MicahHolt2022 <mccormickprophet@gmail.com>
2021-09-14 09:33:26 -0500

    Defining film

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-#print first favorite movie
-print("Rogue One")
-#print second favorite movie
-print("La La Land")
-#print third favorite movie
-print("Megamind")
-#print fourth favorite movie
-print("The Empire Strikes Back")
-#print fifth favorite movie
-print("Knives Out")
+#defining my five favorite films
+def fiveFavFilms():
+    #print first favorite movie
+    print("Rogue One")
+    #print second favorite movie
+    print("La La Land")
+    #print third favorite movie
+    print("Megamind")
+    #print fourth favorite movie
+    print("The Empire Strikes Back")
+    #print fifth favorite movie
+    print("Knives Out")
+#printing title
+print("These are my five favorite movies!")
+#running shortcut fiveFavFilms
+fiveFavFilms()
