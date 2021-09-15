b48c8a3aca6ebb1dd3fcd63d2008195782c7724c
MicahHolt2022 <mccormickprophet@gmail.com>
2021-09-15 09:14:10 -0500

    Five more functions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#defining my functions
 #defining my five favorite films
 def fiveFavFilms():
     #print first favorite movie
@@ -10,7 +11,61 @@ def fiveFavFilms():
     print("The Empire Strikes Back")
     #print fifth favorite movie
     print("Knives Out")
+#defining snakeEater
+def snakeEater():
+    #clanking
+    print("clank")
+    print("clank")
+    print("clank")
+    print("clank")
+    print("clank")
+    #saying the first line
+    print("What a thrill...")
+    #clanking
+    print("clank")
+    print("clank")
+    print("clank")
+    print("clank")
+    print("clank")
+    print("clank")
+#defining mySiblings
+def mySiblings():
+    #print oldest to youngest, my siblings
+    print("Caleb")
+    print("Susanna")
+    print("Grace")
+    print("Isak")
+    print("Phoebe")
+    print("Magdalena")
+#defining orderSixtySix
+def orderSixtySix():
+    #print Palpatine's line
+    print("Commander Cody, the time has come. Execute Order Sixty-Six.")
+    #print Cody's line
+    print("It will be done, my lord.")
+#defining my favorite colors
+def favoriteColors():
+    #print best colors
+    print("Dark Green")
+    print("Royal Purple")
+    print("Black")
+#defining bestTeacher
+def bestTeacher():
+    #printing the question
+    print("And the best teacher is...")
+    #printing mr osowski
+    print("Mr Osowski!")
 #printing title
 print("These are my five favorite movies!")
 #running shortcut fiveFavFilms
 fiveFavFilms()
+#running snakeEater
+snakeEater()
+#running mySiblings
+mySiblings()
+#running orderSixtySix
+orderSixtySix()
+#running favoriteColors
+favoriteColors()
+#running bestTeacher
+bestTeacher()
