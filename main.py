c78000cca861a9a4d135be4bc31dd7d99daad818
MicahHolt2022 <mccormickprophet@gmail.com>
2021-09-17 09:02:15 -0500

    madlibs part two

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 def madLib(adjective,adjectiveA,typeOfBird,roomInAHouse,verbPast,verb,relativeName,nounA,liquid,verbIngA,partOfTheBodyPlural,pluralNoun,verbIngB,nounB):
     #set output
-    print("It was a " + adjective + ", cold  November day. ")
+    print("It was a " + adjective + ", cold November day. ")
     print("I woke up to the " + adjectiveA + " smell of " + typeOfBird + " roasting in the " + roomInAHouse + " downstairs. ")
     print("I " + verbPast + " down the stairs to see if I could help " + verb + " the dinner. ")
     print("My mom said, ..See if " + relativeName + " needs a fresh " + nounA + "..." )
@@ -10,4 +10,5 @@ def madLib(adjective,adjectiveA,typeOfBird,roomInAHouse,verbPast,verb,relativeNa
     print("When I got there, I couldn't believe my " + partOfTheBodyPlural + "! ")
     print("There were " + pluralNoun + " " + verbIngB + " on the " + nounB + "!")
 #run madLib(adjective,adjectiveA,typeOfBird,roomInAHouse,verbPast,verb,relativeName,noun,liquid,verbIngA,partOfTheBodyPlural,pluralNoun,verbIngB,nounB)
-madLib("funny","stupid","eagle","bathroom","sucked","slurp","Caleb","chair","OJ","jumping","thighs","chickens","swinging","Honda Civic")
+madLib("smelly","disgusting","toucan","bathroom","waited","punch","Mr Osowski","zoo","Bang Energy","sleeping","hands","bazookas","screaming","Jeffery Bezos head platter")
+
