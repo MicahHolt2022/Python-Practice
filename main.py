524039b14bea4a6f847a9386f0f31742bc209f9c
MicahHolt2022 <mccormickprophet@gmail.com>
2021-09-20 09:14:05 -0500

    return practice (woof)

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-#define
-
-def madLib(adjective,adjectiveA,typeOfBird,roomInAHouse,verbPast,verb,relativeName,nounA,liquid,verbIngA,partOfTheBodyPlural,pluralNoun,verbIngB,nounB):
-    #set output
-    print("It was a " + adjective + ", cold November day. ")
-    print("I woke up to the " + adjectiveA + " smell of " + typeOfBird + " roasting in the " + roomInAHouse + " downstairs. ")
-    print("I " + verbPast + " down the stairs to see if I could help " + verb + " the dinner. ")
-    print('My mom said, "See if ' + relativeName + ' needs a fresh ' + nounA + '."' )
-    print("So I carried a tray of glasses full of " + liquid + " into the " + verbIngA + " room. ")
-    print("When I got there, I couldn't believe my " + partOfTheBodyPlural + "! ")
-    print("There were " + pluralNoun + " " + verbIngB + " on the " + nounB + "!")
-#run madLib(adjective,adjectiveA,typeOfBird,roomInAHouse,verbPast,verb,relativeName,noun,liquid,verbIngA,partOfTheBodyPlural,pluralNoun,verbIngB,nounB)
-madLib("smelly","disgusting","toucan","bathroom","waited","punch","Mr Osowski","zoo","Bang Energy","sleeping","hands","bazookas","screaming","Jeffery Bezos head platter")
-
+#defining woof
+def woof():
+    #defining substitute
+    return "Woof!"
+#printing woof()'s substitute
+print(woof())
