3b22f0d2364bfe36079255e909b08ca9d2a3b324
MicahHolt2022 <mccormickprophet@gmail.com>
2021-09-28 09:16:02 -0500

    Adjustments

--- side.py
+++ side.py
@@ -143,3 +143,9 @@ seed(1)
 for _ in range(1):
     value = randint(0,100)
     print(value)
+
+def functionWord(word):
+    word = word.lower
+    if word == "yellow":
+        return "cool"
+print(functionWord("YeLLOw"))
